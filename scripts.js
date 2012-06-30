ae9bd3b26279ddda891297449e4d450a06a331ed
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2012-06-30 17:09:33 -0300

    Added a text over the image.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 
 $(function() {
-	$(".content-box>*").hide();
-	$(".content-box>*").fadeIn('slow', function() {} );
+	$(".image-label").hide();
+	
+	$("img").bind("mouseover", showImageLabel);
+	$("img").bind("click", showImageLabel);
+	$("img").bind("mouseleave", hideImageLabel);
+	
+	function showImageLabel() {
+		$(".image-label").fadeIn('fast');
+	}
+	
+	function hideImageLabel() {
+		$(".image-label").fadeOut('fast');
+	}
 });
