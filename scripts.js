c8069b45059200bfc46fd061ba5f6fd90a12171d
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2012-07-03 20:52:22 -0300

    Trying to fix the borders problem of images on IE.

--- scripts.js
+++ scripts.js
@@ -98,13 +98,13 @@
 })();
 
 $(window).load(function() {
-  $('.image-label').hide();
-  $('.image-area').show();
+	$('.image-label').hide();
+	$('.image-area').show();
 	$('.image-box')
 	  .on('mouseenter', function() {
 		  $(this).find('.image-label').fadeIn('fast');
 	  })
 	  .on('mouseleave', function() {
 		  $(this).find('.image-label').fadeOut('fast');
-	});
+	  });
 });
